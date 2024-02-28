b3bae5d1999176c7675eb745d12bc854c39fcada
BankkV <99808324+Jirayubank@users.noreply.github.com>
2024-02-28 15:08:50 +0700

    Add files via upload

---
+++ basicInference.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+# model = YOLO('model_- 26 february 2024 12_22.pt', task='detect')
+model = YOLO('model_- 26 february 2024 12_22.onnx', task='detect')
+dataStreaming = 'rtsp://admin:tatc1234@192.168.1.64:554/Streaming/Channels/101'
+model.predict(source=dataStreaming, show=True, imgsz=416)
---
+++ intermedateInference.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import supervision as sv
+# model = YOLO('model_- 26 february 2024 12_22.pt', task='detect')
+model = YOLO('model_- 26 february 2024 12_22.onnx', task='detect')
+
+for result in model.predict(source=0, show=True, imgsz=416, stream=True):
+    detection = sv.Detections.from_ultralytics(result)
+    labels = [
+        f"detection : {result.names[class_id]}"
+        for _, _, _, class_id, _
+        in detection
+    ]
+    print(labels)

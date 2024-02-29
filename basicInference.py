45eced20c473b1a1dab3b0d94291e8c59c10bb9f
BvoquE_101 <jirayubank10@gmail.com>
2024-02-29 12:58:16 +0700

    upload testimg

--- basicInference.py
+++ basicInference.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('folder/model_- 26 february 2024 12_22.onnx', task='detect')
-dataStreaming = 'rtsp://admin:tatc1234@192.168.1.64:554/Streaming/Channels/101'
-model.predict(source=dataStreaming, show=True, imgsz=416)
+model = YOLO('elecCompo-2902Ver1.pt', task='detect')
+# dataStreaming = 'rtsp://admin:tatc1234@192.168.1.64:554/Streaming/Channels/101'
+
+model.predict(source='testimg45jpg.jpg', show=True, save=True, imgsz=640)
+

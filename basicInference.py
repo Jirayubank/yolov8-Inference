f973a08ac0dc6aa5e23387bfcc0b12d4624c92f2
BvoquE_101 <jirayubank10@gmail.com>
2024-03-06 09:18:10 +0700

    update project

--- basicInference.py
+++ basicInference.py
@@ -1,7 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO('elecCompo-2902Ver1.pt', task='detect')
+model = YOLO('models/eleccomponent-2902ver2.pt', task='detect')
 # dataStreaming = 'rtsp://admin:tatc1234@192.168.1.64:554/Streaming/Channels/101'
 
-model.predict(source='testimg45jpg.jpg', show=True, save=True, imgsz=640)
-
+for result in model.predict(
+    source='testImg/testimg1.jpg',
+    conf=0.5,
+    show=True,
+    save=True,
+    classes=[0, 1],
+    imgsz=(640, 640),
+    agnostic_nms=True
+):
+    print(result)
